2ec831779bbb7e283dbfb952a6c619dcc110be9b
James Skemp <strivinglife@gmail.com>
2014-10-03 21:19:22 -0500

    Message now displayed on second page of application.

--- MyFirstApp/app/src/main/java/com/jamesrskemp/myfirstapp/MyActivity.java
+++ MyFirstApp/app/src/main/java/com/jamesrskemp/myfirstapp/MyActivity.java
@@ -1,14 +1,18 @@
 package com.jamesrskemp.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MyActivity extends ActionBarActivity {
+	public final static String EXTRA_MESSAGE = "com.jamesrskemp.myfirstapp.MESSAGE";
 
-    @Override
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
@@ -33,4 +37,13 @@ public class MyActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+	public void sendMessage(View view) {
+		// Called from activity_my when user clicks to send a message.
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText)findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+	}
 }
