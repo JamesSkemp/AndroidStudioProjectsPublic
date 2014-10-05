2be723ffb9d92217d6f5b98bc06df37e50cea1a5
James Skemp <strivinglife@gmail.com>
2014-10-04 21:07:04 -0500

    Added basic functionality to check for Bluetooth, and enable/disable it.

--- BluetoothTesting/app/src/main/java/com/jamesrskemp/bluetoothtesting/MainActivity.java
+++ BluetoothTesting/app/src/main/java/com/jamesrskemp/bluetoothtesting/MainActivity.java
@@ -1,19 +1,33 @@
 package com.jamesrskemp.bluetoothtesting;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+	// Requesting to enable Bluetooth
+	static final int REQUEST_ENABLE_BT = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+	    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+	    TextView textView = (TextView)findViewById(R.id.textView);
+	    if (bluetoothAdapter == null) {
+		    textView.setText("Bluetooth is not supported.");
+	    } else {
+		    textView.setText("Bluetooth is supported.");
+	    }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,4 +47,29 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+	public void enableBluetooth(View view) {
+		BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (bluetoothAdapter != null && !bluetoothAdapter.isEnabled())
+		{
+			Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BT);
+		}
+	}
+
+	public void disableBluetooth(View view) {
+		BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (bluetoothAdapter != null && bluetoothAdapter.isEnabled()) {
+			bluetoothAdapter.disable();
+			Toast.makeText(this, "Bluetooth disabled", Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_ENABLE_BT) {
+			if (resultCode == RESULT_OK) {
+				Toast.makeText(this, "Bluetooth enabled", Toast.LENGTH_SHORT).show();
+			}
+		}
+	}
 }
