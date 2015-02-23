c35357b3a3ac09a12c8ceb71ce338ad6b77c9586
James Skemp <strivinglife@gmail.com>
2015-02-22 20:56:34 -0600

    Initial button listing can now be scrolled through.

--- FirstOpenGLProject/app/src/main/java/com/jamesrskemp/firstopenglproject/MainActivity.java
+++ FirstOpenGLProject/app/src/main/java/com/jamesrskemp/firstopenglproject/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 
@@ -141,7 +142,10 @@ public class MainActivity extends ActionBarActivity {
 		linearLayout.addView(b8,
 				new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
 
-		this.addContentView(linearLayout, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+		ScrollView scrollView = new ScrollView(this);
+		scrollView.addView(linearLayout, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
+		this.addContentView(scrollView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
 	}
 
 	@Override
