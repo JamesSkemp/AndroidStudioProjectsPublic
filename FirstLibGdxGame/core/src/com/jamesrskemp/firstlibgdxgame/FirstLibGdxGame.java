329ad5ee72a73f2b16dffe2adf4d08819b31e0fa
James Skemp <strivinglife@gmail.com>
2015-03-02 15:32:25 -0600

    Game now displays static background image.

--- FirstLibGdxGame/core/src/com/jamesrskemp/firstlibgdxgame/FirstLibGdxGame.java
+++ FirstLibGdxGame/core/src/com/jamesrskemp/firstlibgdxgame/FirstLibGdxGame.java
@@ -3,34 +3,50 @@ package com.jamesrskemp.firstlibgdxgame;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FirstLibGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	BitmapFont font;
+	FPSLogger fpsLogger;
+	OrthographicCamera camera;
+	Texture background;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		font = new BitmapFont(Gdx.files.internal("verdana39.fnt"), Gdx.files.internal("verdana39.png"), false);
-		font.setColor(Color.RED);
+		fpsLogger = new FPSLogger();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+		background = new Texture("background.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		font.draw(batch, "Hello world", 200, 200);
-		batch.end();
+		fpsLogger.log();
+		updateScene();
+		drawScene();
 	}
 
 	@Override
 	public void dispose() {
 		batch.dispose();
-		font.dispose();
+	}
+
+	public void updateScene() {
+	}
+
+	public void drawScene() {
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(background, 0, 0);
+		batch.end();
 	}
 }
