302efdfb729ee6f2848b122c38b32f26fb8a34b3
James Skemp <strivinglife@gmail.com>
2015-03-02 11:14:03 -0600

    Now displaying text as the 'game.'

--- FirstLibGdxGame/core/src/com/jamesrskemp/firstlibgdxgame/FirstLibGdxGame.java
+++ FirstLibGdxGame/core/src/com/jamesrskemp/firstlibgdxgame/FirstLibGdxGame.java
@@ -2,26 +2,35 @@ package com.jamesrskemp.firstlibgdxgame;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FirstLibGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	BitmapFont font;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		font = new BitmapFont(Gdx.files.internal("verdana39.fnt"), Gdx.files.internal("verdana39.png"), false);
+		font.setColor(Color.RED);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		font.draw(batch, "Hello world", 200, 200);
 		batch.end();
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+		font.dispose();
 	}
 }
