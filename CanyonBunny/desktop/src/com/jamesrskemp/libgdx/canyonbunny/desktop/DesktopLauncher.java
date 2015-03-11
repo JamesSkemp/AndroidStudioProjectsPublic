afe9b1993fdf054c45efc398df8d94f0a5a02126
James Skemp <strivinglife@gmail.com>
2015-03-10 19:42:22 -0500

    Started on project from Learning LibGDX Game Development Second Edition.

---
+++ CanyonBunny/desktop/src/com/jamesrskemp/libgdx/canyonbunny/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jamesrskemp.libgdx.canyonbunny.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jamesrskemp.libgdx.canyonbunny.CanyonBunny;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CanyonBunny(), config);
+	}
+}
