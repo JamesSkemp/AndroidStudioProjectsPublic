5d32f2dbbe2226f4924c592820f13fd4744d768a
James Skemp <strivinglife@gmail.com>
2015-03-13 18:33:20 -0500

    Configure desktop application with correct size and title.

--- Drop/desktop/src/com/jamesrskemp/libgdx/drop/desktop/DesktopLauncher.java
+++ Drop/desktop/src/com/jamesrskemp/libgdx/drop/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.jamesrskemp.libgdx.drop.DropGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 480;
+		config.title = "Drop";
 		new LwjglApplication(new DropGame(), config);
 	}
 }
