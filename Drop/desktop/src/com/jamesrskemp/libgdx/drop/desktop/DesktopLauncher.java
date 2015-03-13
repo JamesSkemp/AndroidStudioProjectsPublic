793fc1ca8d25f786a3501b4cf824ad5e638d1f3e
James Skemp <strivinglife@gmail.com>
2015-03-13 18:23:24 -0500

    Started on simple game example from official libGDX wiki.

---
+++ Drop/desktop/src/com/jamesrskemp/libgdx/drop/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jamesrskemp.libgdx.drop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jamesrskemp.libgdx.drop.DropGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DropGame(), config);
+	}
+}
