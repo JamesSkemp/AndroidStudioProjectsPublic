d26c3b4a3903286fef01830c4b7084b6b7aadcb0
James Skemp <strivinglife@gmail.com>
2015-03-13 21:47:39 -0500

    Removed default code and asset, while adding new assets and enough code to render the bucket.

--- Drop/core/src/com/jamesrskemp/libgdx/drop/DropGame.java
+++ Drop/core/src/com/jamesrskemp/libgdx/drop/DropGame.java
@@ -2,26 +2,64 @@ package com.jamesrskemp.libgdx.drop;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class DropGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private Texture dropImage;
+	private Texture bucketImage;
+	private Sound dropSound;
+	private Music rainMusic;
+
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+
+	private Rectangle bucket;
+
 	@Override
 	public void create () {
+		dropImage = new Texture("droplet.png");
+		bucketImage = new Texture("bucket.png");
+
+		// Sounds are loaded in memory, and should be < 10 seconds.
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("30341__junggle__waterdrop24.wav"));
+		// Music is streamed from where it's stored.
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("28283__acclivity__undertreeinrain.mp3"));
+
+		rainMusic.setLooping(true);
+		rainMusic.play();
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		// Bottom left corner is 0, 0.
+		// Our bucket image is 64x64 pixels.
+		bucket = new Rectangle();
+		bucket.x = 800 / 2 - 64 / 2;
+		bucket.y = 20;
+		bucket.width = 64;
+		bucket.height = 64;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		// Good practice to update the camera once per frame.
+		camera.update();
+
+		batch.setProjectionMatrix(camera.combined);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(bucketImage, bucket.x, bucket.y);
 		batch.end();
 	}
 }
