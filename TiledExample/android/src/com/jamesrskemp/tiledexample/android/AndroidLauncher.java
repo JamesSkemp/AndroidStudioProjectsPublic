ba558c1d46b665d3edc080dc0f483f4900a5e9a0
James Skemp <strivinglife@gmail.com>
2015-03-20 11:04:45 -0500

    Started on new project to test Tiled tmx imports.

---
+++ TiledExample/android/src/com/jamesrskemp/tiledexample/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jamesrskemp.tiledexample.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jamesrskemp.tiledexample.TiledExample;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TiledExample(), config);
+	}
+}
