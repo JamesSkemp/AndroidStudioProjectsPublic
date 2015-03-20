69cb123f7b57796b9f56f092286632c0408e3600
James Skemp <strivinglife@gmail.com>
2015-03-20 14:00:39 -0500

    Added more setup steps to Tiled Example application.

--- TiledExample/core/src/com/jamesrskemp/tiledexample/TiledExample.java
+++ TiledExample/core/src/com/jamesrskemp/tiledexample/TiledExample.java
@@ -3,16 +3,21 @@ package com.jamesrskemp.tiledexample;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class TiledExample extends ApplicationAdapter {
 	SpriteBatch batch;
+	OrthographicCamera camera;
+
 	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
 		//img = new Texture("badlogic.jpg");
 	}
 
@@ -20,8 +25,17 @@ public class TiledExample extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+		
 		batch.begin();
 		//batch.draw(img, 0, 0);
 		batch.end();
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
 	}
 }
