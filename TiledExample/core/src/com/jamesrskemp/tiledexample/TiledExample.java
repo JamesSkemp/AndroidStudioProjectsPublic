f1b7b8700f1c4d0410221cd24b94f481ba24c0af
James Skemp <strivinglife@gmail.com>
2015-03-20 19:13:01 -0500

    Can now move the map around by touching. Left code that doesn't work that extends gestures.

--- TiledExample/core/src/com/jamesrskemp/tiledexample/TiledExample.java
+++ TiledExample/core/src/com/jamesrskemp/tiledexample/TiledExample.java
@@ -6,33 +6,54 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
+import com.jamesrskemp.tiledexample.util.OrthoCamController;
 
 public class TiledExample extends ApplicationAdapter {
 	SpriteBatch batch;
 	OrthographicCamera camera;
+	OrthoCamController cameraController;
 
 	TiledMap map;
 	TiledMapTileLayer layer;
 	IsometricTiledMapRenderer renderer;
+	//OrthogonalTiledMapRenderer renderer2;
 	Texture img;
 	
 	@Override
 	public void create () {
+		float screenWidth = Gdx.graphics.getWidth();
+		float screenHeight = Gdx.graphics.getHeight();
+		float cellsInRow = 5 * 64;
+
 		batch = new SpriteBatch();
+
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
+		camera.setToOrtho(false, cellsInRow * screenWidth / screenHeight, cellsInRow);
+
+		cameraController = new OrthoCamController(camera);
+		//Gdx.input.setInputProcessor(new GestureDetector(cameraController));
+		Gdx.input.setInputProcessor(cameraController);
+
 		map = new TmxMapLoader().load("SimpleCityTutorialMap.tmx");
 		layer = (TiledMapTileLayer)map.getLayers().get(0);
 
-		renderer = new IsometricTiledMapRenderer(map, 0.5f, batch);
-		renderer.setView(camera);
+		//camera.position.set(layer.getWidth() * 128 / 2, layer.getHeight() * 64 / 4, 0);
+		camera.position.set(screenWidth / 2, screenHeight / 2, 0);
+		Gdx.app.log(TiledExample.class.getName(), "Camera: <" + camera.position.x + "," + camera.position.y + ">");
+		camera.position.set(layer.getWidth() * layer.getTileWidth() / 2, layer.getHeight() * layer.getTileHeight() / 2, 0);
+		Gdx.app.log(TiledExample.class.getName(), "Camera: <" + camera.position.x + "," + camera.position.y + ">");
+
+		renderer = new IsometricTiledMapRenderer(map, 1f, batch);
+		//renderer2 = new OrthogonalTiledMapRenderer(map, batch);
 
 		Gdx.app.log(TiledExample.class.getName(), "Map: <" + layer.getWidth() + "," + layer.getHeight() + ">");
 		Gdx.app.log(TiledExample.class.getName(), "Tile: <" + layer.getTileWidth() + "," + layer.getTileHeight() + ">");
+		Gdx.app.log(TiledExample.class.getName(), "Camera: <" + camera.position.x + "," + camera.position.y + ">");
 
 		//img = new Texture("badlogic.jpg");
 	}
@@ -45,6 +66,7 @@ public class TiledExample extends ApplicationAdapter {
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
 
+		renderer.setView(camera);
 		renderer.render();
 		batch.begin();
 		//
@@ -55,5 +77,7 @@ public class TiledExample extends ApplicationAdapter {
 	public void dispose() {
 		batch.dispose();
 		map.dispose();
+		renderer.dispose();
+		//renderer2.dispose();
 	}
 }
--- TiledExample/core/src/com/jamesrskemp/tiledexample/util/OrthoCamController.java
+++ TiledExample/core/src/com/jamesrskemp/tiledexample/util/OrthoCamController.java
@@ -1,13 +1,19 @@
 package com.jamesrskemp.tiledexample.util;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
 /**
  * Created by James on 3/20/2015.
  */
 public class OrthoCamController extends InputAdapter {
+	// was extending from GestureDetector.GestureAdapter
+	private static final String TAG = OrthoCamController.class.getName();
+
 	final OrthographicCamera camera;
 	final Vector3 curr = new Vector3();
 	final Vector3 last = new Vector3(-1, -1, -1);
@@ -17,7 +23,14 @@ public class OrthoCamController extends InputAdapter {
 		this.camera = camera;
 	}
 
-	@Override
+	//@Override
+	public boolean panStop(float x, float y, int pointer, int button) {
+		Gdx.app.log(TAG, "panStop");
+		camera.position.set(x, y, 0);
+		return false;
+	}
+
+	//@Override
 	public boolean touchDragged (int x, int y, int pointer) {
 		camera.unproject(curr.set(x, y, 0));
 		if (!(last.x == -1 && last.y == -1 && last.z == -1)) {
@@ -29,9 +42,81 @@ public class OrthoCamController extends InputAdapter {
 		return false;
 	}
 
-	@Override
+	//@Override
 	public boolean touchUp (int x, int y, int pointer, int button) {
+		Gdx.app.log(TAG, "touchUp");
 		last.set(-1, -1, -1);
+		return false;
+	}
+
+	//@Override
+	public boolean zoom(float initialDistance, float distance) {
+		Gdx.app.log(TAG, "zoom");
+		// TODO
+		return false;
+	}
+
+	//@Override
+	public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+		Gdx.app.log(TAG, "pinch");
+		// TODO
+		return false;
+	}
+
+	//@Override
+	public boolean fling(float velocityX, float velocityY, int button) {
+		Gdx.app.log(TAG, "fling");
+		// TODO
+		return false;
+	}
+
+	//@Override
+	public boolean longPress(float x, float y) {
+		Gdx.app.log(TAG, "longPress");
+		// TODO
+		return false;
+	}
+
+	//@Override
+	public boolean touchDown(float x, float y, int pointer, int button) {
+		Gdx.app.log(TAG, "touchDown");
+		// TODO
+		return false;
+	}
+
+	//@Override
+	public boolean tap(float x, float y, int count, int button) {
+		Gdx.app.log(TAG, "tap");
+		// TODO
+		return false;
+	}
+
+	//@Override
+	public boolean pan(float x, float y, float deltaX, float deltaY) {
+		Gdx.app.log(TAG, "Camera: <" + camera.position.x + "," + camera.position.y + "> zoom: " + camera.zoom);
+		Gdx.app.log(TAG, "Current before: <" + x + "," + y + ">");
+		//camera.unproject(curr.set(x, y, 0));
+		Gdx.app.log(TAG, "Current after: <" + x + "," + y + ">");
+		//Gdx.app.log(TAG, "Current 2: <" + curr.x + "," + curr.y + ">");
+		Gdx.app.log(TAG, "Delta: <" + deltaX + "," + deltaY + ">");
+		Gdx.app.log(TAG, "Delta 2: <" + delta.x + "," + delta.y + ">");
+		//camera.unproject(delta.set(deltaX, deltaY, 0));
+		//Gdx.app.log(TAG, "Last: <" + last.x + "," + last.y + ">");
+		/*if (!(last.x == -1 && last.y == -1 && last.z == -1)) {
+			//camera.unproject(delta.set(last.x, last.y, 0));
+			delta.sub(curr);
+			camera.position.add(delta.x, delta.y, 0);
+		}*/
+		//camera.unproject(delta.set(deltaX, deltaY, 0));
+		camera.position.add(-delta.x * camera.zoom, delta.y * camera.zoom, 0);
+
+		if (camera.position.x > 1000 || camera.position.x < -1000) {
+			camera.position.x = 0;
+		}
+		if (camera.position.y > 1000 || camera.position.y < -1000) {
+			camera.position.y = 0;
+		}
+		//last.set(x, y, 0);
 		return false;
 	}
 }
