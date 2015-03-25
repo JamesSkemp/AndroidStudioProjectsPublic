05704edff7feb965f59121f526d6a32d26a5c75e
James Skemp <strivinglife@gmail.com>
2015-03-24 21:14:35 -0500

    Created new project to try out the libGDX tools.

---
+++ LibGDXToolsApp/desktop/src/com/jamesrskemp/libgdxToolsApp/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jamesrskemp.libgdxToolsApp.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jamesrskemp.libgdxToolsApp.LibGDXToolsApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LibGDXToolsApp(), config);
+	}
+}
