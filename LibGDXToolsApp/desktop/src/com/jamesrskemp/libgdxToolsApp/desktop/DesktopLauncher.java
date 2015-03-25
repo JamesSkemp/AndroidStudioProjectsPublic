8d2b17f256627f7f1c6877a28464da85a5b55369
James Skemp <strivinglife@gmail.com>
2015-03-24 21:50:29 -0500

    Cleaned up after my attempt at TiledMapPacker.

--- LibGDXToolsApp/desktop/src/com/jamesrskemp/libgdxToolsApp/desktop/DesktopLauncher.java
+++ LibGDXToolsApp/desktop/src/com/jamesrskemp/libgdxToolsApp/desktop/DesktopLauncher.java
@@ -16,26 +16,6 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-		// None of this works.
-		// http://www.badlogicgames.com/forum/viewtopic.php?f=11&t=18377 suggests this is no longer supported.
-		Gdx.app.log(TAG, new File("../").getAbsolutePath());
-
-		Gdx.app.log(TAG, Gdx.files.external("../../../../../").file().getAbsolutePath());
-
-		File sourceDirectory = Gdx.files.internal("originals").file();
-		File packedDirectory = Gdx.files.internal("packed").file();
-		TiledMapPacker tiledMapPacker = new TiledMapPacker();
-
-		Gdx.app.log(TAG, sourceDirectory.getAbsolutePath());
-		Gdx.app.log(TAG, packedDirectory.getAbsolutePath());
-
-		try {
-			tiledMapPacker.processMaps(sourceDirectory, packedDirectory, new TexturePacker.Settings());
-		} catch (IOException e) {
-			Gdx.app.log(TAG, e.getMessage());
-			// do nothing
-		}
-
 		new LwjglApplication(new LibGDXToolsApp(), config);
 	}
 }
